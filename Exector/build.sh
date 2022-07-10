cbc827a1bc28be75fd4dda2c3a653bc0e17e3d2f
C0derByM4H6301 <109034196+C0derByM4H6301@users.noreply.github.com>
2022-07-10 19:23:59 +0300

    Update build.sh

--- Exector/build.sh
+++ Exector/build.sh
@@ -3,6 +3,8 @@ chmod 777 Exector.py
 sudo apt update
 sudo apt install python3 -y
 sudo apt install python3-pip -y
+sudo apt install tor -y
+sudo apt install git -y
 sudo pip3 install colorama
 sudo pip3 install os
 sudo pip3 install time
