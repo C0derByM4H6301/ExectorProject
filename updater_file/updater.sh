a95e550789480b5f5b405595731a4d30614710cc
Mahmut Coder SU <109034196+C0derByM4H6301@users.noreply.github.com>
2022-07-12 13:34:23 +0300

    Add files via upload

---
+++ updater_file/updater.sh
@@ -0,0 +1,5 @@
+#/usr/bin/bash
+echo "updater is started"
+git clone https://github.com/C0derByM4H6301/ExectorProject.git && cd ExectorProject/Exector/ && chmod 777 build.sh && bash build.sh
+echo updated!!
+echo happy hunting
