812471aeb56e403085cda5e88209a4408ed15b85
Mahmut Coder SU <109034196+C0derByM4H6301@users.noreply.github.com>
2022-07-25 17:01:04 +0300

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "updater is started"
+cd ..
+rm -rf ExectorProject
+git clone https://github.com/C0derByM4H6301/ExectorProject.git 
+echo updated!!
+echo happy hunting
+echo "bug found! please type cd .."
