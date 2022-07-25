90c63380d2e4fc87df0a3f87e8af77c3bc89060f
Mahmut Coder SU <109034196+C0derByM4H6301@users.noreply.github.com>
2022-07-25 16:56:26 +0300

    Delete updater_file directory

--- updater_file/updater.sh
+++
@@ -1,5 +0,0 @@
-#/usr/bin/bash
-echo "updater is started"
-git clone https://github.com/C0derByM4H6301/ExectorProject.git && cd ExectorProject/&& chmod 777 build.sh && bash build.sh
-echo updated!!
-echo happy hunting
